4002a46bf8a10cc1aeff77d871337578147b1184
yamada kenta <yamapaper2234@gmail.com>
2025-05-09 16:09:37 +0900

    ボタンの移動（プロパティアクセス）

--- cs0509/Form1.Designer.cs
+++ cs0509/Form1.Designer.cs
@@ -29,13 +29,19 @@
         private void InitializeComponent()
         {
             buttonPropety = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // buttonPropety
             // 
             buttonPropety.AutoSize = true;
             buttonPropety.BackColor = SystemColors.ActiveCaption;
-            buttonPropety.Location = new Point(17, 10);
+            buttonPropety.Location = new Point(1, 0);
             buttonPropety.Name = "buttonPropety";
             buttonPropety.Size = new Size(197, 134);
             buttonPropety.TabIndex = 0;
@@ -43,14 +49,80 @@
             buttonPropety.UseVisualStyleBackColor = false;
             buttonPropety.Click += button1_Click;
             // 
+            // button1
+            // 
+            button1.Location = new Point(1, 140);
+            button1.Name = "button1";
+            button1.Size = new Size(192, 109);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(311, 94);
+            button2.Name = "button2";
+            button2.Size = new Size(84, 49);
+            button2.TabIndex = 2;
+            button2.Text = "up";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(230, 140);
+            button3.Name = "button3";
+            button3.Size = new Size(84, 49);
+            button3.TabIndex = 3;
+            button3.Text = "left";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(392, 140);
+            button4.Name = "button4";
+            button4.Size = new Size(84, 49);
+            button4.TabIndex = 4;
+            button4.Text = "right";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(311, 186);
+            button5.Name = "button5";
+            button5.Size = new Size(84, 49);
+            button5.TabIndex = 5;
+            button5.Text = "down";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(335, 157);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 6;
+            label1.Text = "label1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(buttonPropety);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -58,5 +130,11 @@
         #endregion
 
         private Button buttonPropety;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label1;
     }
 }
--- cs0509/Form1.cs
+++ cs0509/Form1.cs
@@ -2,6 +2,10 @@ namespace cs0509
 {
     public partial class Form1 : Form
     {
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
         public Form1()
         {
             InitializeComponent();
@@ -15,6 +19,85 @@ namespace cs0509
 
             //ìØä˙èàóùÅ@MessageBox.showÇÕìØä˙ä÷êî
             MessageBox.Show("hello");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button1.Left = button1.Left + 1;
+            button1.Top = button1.Top + 1;
+            if (button1.Top == 20 && button1.Left == 20)
+            {
+                button1.Top = 0;
+                button1.Left = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Top -= 1;
+            button3.Left -= 1;
+            button5.Top += 1;
+            button4.Left += 1;
+            count1++;
+            if (count1 == 10)
+            {
+                button2.Top += 10;
+                button3.Top += 10;
+                button5.Top += 10;
+                button4.Top += 10;
+                count1 = 0;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            button2.Top -= 1;
+            button3.Left -= 1;
+            button5.Top += 1;
+            button4.Left += 1;
+            if (count1 == 10)
+            {
+                button2.Top += 10;
+                button3.Top += 10;
+                button5.Top += 10;
+                button4.Top += 10;
+                count1 = 0;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            count2++;
+            if (count2 == 10)
+            {
+               
+                count2 = 0;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            count3++;
+            if (count3 == 10)
+            {
+                
+                count3 = 0;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            count4++;
+            if (count4 == 10)
+            {
+                
+                count4 = 0;
+            }
         }
     }
 }
