e74aa9c08805f71c104ccec7c724b709c4fcb563
yamada kenta <yamapaper2234@gmail.com>
2025-05-09 14:29:13 +0900

    プロパティ

--- cs0509/Form1.Designer.cs
+++ cs0509/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Enabled = false;
+            button1.Location = new Point(17, 10);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 134);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
     }
 }
